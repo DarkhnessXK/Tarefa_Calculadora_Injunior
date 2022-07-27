b342385c95885e7aabbd1c1cf7cd4e81dc68bc55
DarkhnessXK <giovannitoledo@id.uff.br>
2022-07-27 20:44:01 -0300

    fix(calculadora): text log funcional

--- calculadora.js
+++ calculadora.js
@@ -1,21 +1,41 @@
-var i = 0;
-var notas = [];
+let i = 0;
+const notas = [];
 
 function salva_nota() {
-    var btn = document.querySelector("#btn");
-    var text_box = document.getElementById("#box");
+    const btn = document.querySelector("#btn");
     btn.addEventListener("click", () => {
-        notas[i] = document.getElementById("nota_text").value;
-        document.getElementById("#line_box").innerHTML = notas[i];
-        console.log(notas[i]);
-        
-        i += 1;
-        console.log(notas);
-        
+        const div_lista = document.createElement("div");
+        const texto = document.querySelector(".nota_text");
+        const lista_numeros = document.querySelector(".lista");
+        if (texto.value >= 0 && texto.value <= 10) {
+            // console.log(texto.value);
+            div_lista.innerText = `A Nota ${i + 1}: foi ${texto.value} \n`;
+            notas[i] = texto.value
+            lista_numeros.appendChild(div_lista);
+            console.log(notas)
+            i += 1;
+        }
+        else {
+                alert("Digite uma nota valida!!!!")
+            }
 
         
 
     });
+}
+
+function calcular_media() {
+    const media = 0;
+    let soma = 0;
+
+    notas.forEach(nota => {
+        soma += nota;
+    })
+
+    media = soma / notas.length
+
+
+    console.log(media)
 }
 
 salva_nota();

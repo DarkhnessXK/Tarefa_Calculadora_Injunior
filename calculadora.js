13d227282d11999639b7614f9a6df2fcad265cc0
DarkhnessXK <giovannitoledo@id.uff.br>
2022-07-27 22:03:01 -0300

    fix(calculadora): Funcional e tratada

--- calculadora.js
+++ calculadora.js
@@ -7,37 +7,50 @@ function salva_nota() {
         const div_lista = document.createElement("div");
         const texto = document.querySelector(".nota_text");
         const lista_numeros = document.querySelector(".lista");
+        // if(texto.value = ""){   //bugado
+        //     alert("Por favor, Insira uma Nota!!!")
+        // }
         if (texto.value >= 0 && texto.value <= 10) {
-            // console.log(texto.value);
             div_lista.innerText = `A Nota ${i + 1}: foi ${texto.value} \n`;
             notas[i] = texto.value
             lista_numeros.appendChild(div_lista);
-            console.log(notas)
             i += 1;
         }
         else {
-                alert("Digite uma nota valida!!!!")
-            }
+            alert("Digite uma nota valida!!!!")
+        }
 
-        
 
+        return notas;
     });
 }
 
 function calcular_media() {
-    const media = 0;
+    let media = 0;
     let soma = 0;
-
-    notas.forEach(nota => {
-        soma += nota;
+    const media_final = document.querySelector("#media");
+    const btn_calcular = document.querySelector("#calcular")
+    btn_calcular.addEventListener("click", () => {
+        for(let nota of notas){
+            nota = parseFloat(nota);
+            soma = soma + nota;
+        }
+        media = soma / notas.length
+        media = media.toFixed(2)
+        console.log(media)
+        media_final.innerText = media;
+    soma = 0;
+    media = 0;
     })
 
-    media = soma / notas.length
 
 
-    console.log(media)
+
+
+
+
 }
 
 salva_nota();
-
+calcular_media();
 
